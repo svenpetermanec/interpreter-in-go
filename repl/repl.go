f414afe78101d1019084619118343ab7a7fdbfb9
svenpetermanec <sven.petermanec@axilis.com>
2024-09-25 20:05:38 +0200

    support file interpretation

--- repl/repl.go
+++ repl/repl.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"os"
 
 	"interpreter/evaluator"
 	"interpreter/lexer"
@@ -18,6 +19,16 @@ func Start(in io.Reader, out io.Writer) {
 	env := object.NewEnvironment()
 	macroEnv := object.NewEnvironment()
 
+	if len(os.Args) > 1 {
+		file, err := os.ReadFile(os.Args[1])
+		if err != nil {
+			panic(err)
+		}
+
+		repl(string(file), out, env, macroEnv)
+		return
+	}
+
 	for {
 		fmt.Printf(PROMPT)
 		scanned := scanner.Scan()
@@ -26,23 +37,28 @@ func Start(in io.Reader, out io.Writer) {
 		}
 
 		line := scanner.Text()
-		l := lexer.New(line)
-		p := parser.New(l)
 
-		program := p.ParseProgram()
-		if len(p.Errors()) != 0 {
-			printParserErrors(out, p.Errors())
-			continue
-		}
+		repl(line, out, env, macroEnv)
+	}
+}
 
-		evaluator.DefineMacros(program, macroEnv)
-		expanded := evaluator.ExpandMacros(program, macroEnv)
+func repl(input string, out io.Writer, env *object.Environment, macroEnv *object.Environment) {
+	l := lexer.New(input)
+	p := parser.New(l)
 
-		evaluated := evaluator.Eval(expanded, env)
-		if evaluated != nil {
-			io.WriteString(out, evaluated.Inspect())
-			io.WriteString(out, "\n")
-		}
+	program := p.ParseProgram()
+	if len(p.Errors()) != 0 {
+		printParserErrors(out, p.Errors())
+		return
+	}
+
+	evaluator.DefineMacros(program, macroEnv)
+	expanded := evaluator.ExpandMacros(program, macroEnv)
+
+	evaluated := evaluator.Eval(expanded, env)
+	if evaluated != nil {
+		io.WriteString(out, evaluated.Inspect())
+		io.WriteString(out, "\n")
 	}
 }
 
